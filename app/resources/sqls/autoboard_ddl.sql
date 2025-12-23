873e703930db9c7cb51c1ce2dc46b80f71c5a075
KimDoYoung <kdy987@gmail.com>
2025-12-23 16:51:15 +0900

    feat: Add initial database schema for core tables and design document.

--- app/resources/sqls/autoboard_ddl.sql
+++ app/resources/sqls/autoboard_ddl.sql
@@ -10,3 +10,47 @@ create table if not exists admin_users(
 -- 이미 존재하면 무시 (OR IGNORE는 SQLite 문법)
 INSERT OR IGNORE INTO admin_users (id, username, password) 
 VALUES (1, 'admin', '$2b$12$uBulCWv7eNuYY8xfp6j4k.3hawRlYSoifJFO.arMQZZINQMWUfkuy');
+
+-- boards
+create table if not exists boards(
+    id integer primary key,
+    name text not null,
+    physical_table_name text not null,
+    note text,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
+
+-- meta_data
+create table if not exists meta_data(
+    id integer primary key,
+    board_id integer not null,
+    name text not null,
+    meta text not null,
+    schema text not null,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
+
+-- files
+create table if not exists files(
+    id integer primary key,
+    base_folder text not null,
+    physical_name text not null,
+    logical_name text not null,
+    size integer not null,
+    mime text not null,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
+
+-- file_match
+create table if not exists file_match(
+    id integer primary key,
+    board_id integer not null,
+    table_id integer not null,
+    file_id integer not null,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
+    
